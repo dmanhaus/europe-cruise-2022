886d1f8cf5140a2c9337eb7df5cfcff5dff9ded0
Dave Hausler <dave.hausler@gmail.com>
2022-10-27 09:10:10 -0400

    add custom vue video component

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import { defineComponent } from 'vue'
+  import { VideoPlayer } from '@videojs-player/vue'
+  import 'video.js/dist/video-js.css'
+
+  export default defineComponent({
+    components: {
+      VideoPlayer
+    }
+  })
+</script>
+
+<template>
+  <video-player
+    src="/your-path/video.mp4"
+    poster="/your-path/poster.jpg"
+    controls
+    :loop="true"
+    :volume="0.6"
+    ...
+    @mounted="..."
+    @ready="..."
+    @play="..."
+    @pause="..."
+    @ended="..."
+    @seeking="..."
+    ...
+  />
+</template>
